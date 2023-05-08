7038345ec9c5c34fbc9c4f6e2fb8a936f92df121
Nathan Heidacker <nathanheidacker@gmail.com>
2023-05-08 19:53:26 +0900

    v0.1.5

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
 import launch
 
-if not launch.is_installed("facefinder==0.1.4"):
-    launch.run_pip("install facefinder==0.1.4", "requirements for FaceFinder extension")
+if not launch.is_installed("facefinder==0.1.5"):
+    launch.run_pip("install facefinder==0.1.5", "requirements for FaceFinder extension")
